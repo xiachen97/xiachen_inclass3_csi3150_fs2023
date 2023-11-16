fa4e7ddc93f629af3106b01d8759756281b3f72c
xiachen97 <xiachen2@oakland.edu>
2023-11-16 13:40:43 -0500

    submit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Counter from '../src/components/Counter';
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./counter.css";
+const CounterComponent = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div className="counter-container">
+      <div className="counter-box counter-value"><button>{counter}</button></div>
+      <div className="counter-box counter-buttons">
+        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default CounterComponent;
